f1ce4a66fa7ccd75ac1990003940f6a70143d5d3
vanessa <98173531+vnsa-abn@users.noreply.github.com>
2024-03-25 22:21:19 -0400

    Update assessment.py

--- App/controllers/assessment.py
+++ App/controllers/assessment.py
@@ -1,5 +1,5 @@
-from App.models import assessment
+from App.models import Assessment
 from App.database import db
 
 def list_assessment_types():
-    return assessment.query.all() 
+    return Assessment.query.all() 
